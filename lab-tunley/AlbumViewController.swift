bc150e4ff2eccf2f95cb1e6c8de46d356db38093
Tile561 <james.q.merilien@gmail.com>
2023-09-17 18:09:48 -0400

    Setup collection view

--- lab-tunley/AlbumViewController.swift
+++ lab-tunley/AlbumViewController.swift
@@ -6,12 +6,72 @@
 //
 
 import UIKit
+import Nuke
 
-class AlbumViewController: UIViewController {
 
+class AlbumViewController: UIViewController, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // the number of items shown should be the number of albums we have.
+            albums.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // Get a collection view cell (based in the identifier you set in storyboard) and cast it to our custom AlbumCell
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AlbumCell", for: indexPath) as! AlbumCell
+
+            // Use the indexPath.item to index into the albums array to get the corresponding album
+            let album = albums[indexPath.item]
+
+            // Get the artwork image url
+            let imageUrl = album.artworkUrl100
+
+            // Set the image on the image view of the cell
+            Nuke.loadImage(with: imageUrl, into: cell.albumImageView)
+
+            return cell
+    }
+    
+    
+    @IBOutlet weak var collectionview: UICollectionView!
+    
+    
+    var albums: [Album] = []
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionview.dataSource = self
+        // Create a search URL for fetching albums (`entity=album`)
+        let url = URL(string: "https://itunes.apple.com/search?term=blackpink&attribute=artistTerm&entity=album&media=music")!
+        let request = URLRequest(url: url)
 
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+
+            // Handle any errors
+            if let error = error {
+                print("❌ Network error: \(error.localizedDescription)")
+            }
+
+            // Make sure we have data
+            guard let data = data else {
+                print("❌ Data is nil")
+                return
+            }
+
+            let decoder = JSONDecoder()
+            do {
+                // Try to parse the response into our custom model
+                let response = try decoder.decode(AlbumSearchResponse.self, from: data)
+                let albums = response.results
+                DispatchQueue.main.async {
+                    self?.albums = albums
+                    self?.collectionview.reloadData()
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+
+        // Initiate the network request
+        task.resume()
         // Do any additional setup after loading the view.
     }
     
